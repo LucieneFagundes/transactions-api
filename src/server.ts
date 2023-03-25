cec7f8f9c1eeb13f4167fda63766260df28b7400
LucieneFagundes <lfnascimento99@gmail.com>
2023-03-25 13:46:51 -0300

    structure and db implementation

---
+++ src/server.ts
@@ -0,0 +1,28 @@
+import fastify from 'fastify'
+// import crypto from 'node:crypto'
+import { knex } from './database'
+import { env } from './env'
+
+const app = fastify()
+
+app.get('/hello', async () => {
+  const transactions = await knex('transactions').select('*')
+
+  // const transactions = await knex('transactions')
+  //   .insert({
+  //     id: crypto.randomUUID(),
+  //     title: 'Transação de teste',
+  //     amount: 1000,
+  //   })
+  //   .returning('*')
+
+  return transactions
+})
+
+app
+  .listen({
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log(`listening on port: ${env.PORT}`)
+  })
